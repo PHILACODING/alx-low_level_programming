4c4a608abfe8962fcf2d422b2969974a433bbe83
philacoding <philani.ph.msomi@gmail.com>
2022-10-19 07:33:04 -0700

    fixed

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+/**
+ * 3_islower - checks if the charater is lowercase 
+ * Return: returns 1 or 0 depending on condition
+ */
+int _islower(int c)
+{
+	return ((c >= 'a' &&  c <= 'z'));
+}
